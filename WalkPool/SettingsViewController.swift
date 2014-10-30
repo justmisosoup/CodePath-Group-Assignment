1d819596be51b67456313dc95c66b763b7cd226f
“Corin <corin.nader@gmail.comgit config --global user.name Corin Nadergit config --global user.email corin.nader@gmail.com>
2014-10-29 17:38:50 -0700

    Settings page

---
+++ WalkPool/SettingsViewController.swift
@@ -0,0 +1,38 @@
+//
+//  SettingsViewController.swift
+//  WalkPool
+//
+//  Created by Corin Nader on 10/29/14.
+//  Copyright (c) 2014 Sara Menefee. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func onCloseButton(sender: UIButton) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
